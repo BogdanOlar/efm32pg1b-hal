c5d1ad43c1d49168a725f8ce16e081b3281aa34a
Bogdan Olar <olar.bogdan.dev@gmail.com>
2026-04-24 23:20:11 +0300

    Add Peripheral single-cycle read-modify-write blanked inplementation for all PAC registers

--- src/lib.rs
+++ src/lib.rs
@@ -53,3 +53,44 @@ pub mod prelude {
     };
     pub use fugit::RateExtU32;
 }
+
+/// Peripheral single-cycle read-modify-write
+///
+/// The EFM32 Gecko supports bit set and bit clear access to all peripherals except those listed in
+/// Table 4.1 Peripherals that Do Not Support Bit Set and Bit Clear on page 38. The bit set and bit clear functionality
+/// (also called Bit Access) enables modification of bit fields (single bit or multiple bit wide) without the need to
+/// perform a read-modify-write (though it is functionally equivalent). Also, the operation is contained within a single
+/// bus access (for HF peripherals), unlike the Bit-banding operation described in section 4.2.2 Bit-banding which
+/// consumes two bus accesses per operation. All AHB masters can utilize this feature.
+///
+/// See [Documentation](../../doc/efm32pg1-rm.pdf#page919)
+trait SingleCycleRMW {
+    const BIT_CLEAR_BASE_ADDR: usize = 0x44000000;
+    const BIT_SET_BASE_ADDR: usize = 0x46000000;
+    const PERIPHERALS_BASE_ADDR: usize = 0x40000000;
+
+    /// Single cycle bit(s) set
+    ///
+    /// **WARNING**: don't use this for **EMU**, **RMU**, and **CRYOTIMER** peripheral registers!
+    fn sc_set(&self, mask: u32);
+
+    /// Single cycle bit(s) clear
+    ///
+    /// **WARNING**: don't use this for **EMU**, **RMU**, and **CRYOTIMER** peripheral registers!
+    fn sc_clear(&self, mask: u32);
+}
+
+impl<R> SingleCycleRMW for crate::pac::generic::Reg<R>
+where
+    R: crate::pac::generic::RegisterSpec,
+{
+    fn sc_set(&self, mask: u32) {
+        let addr = Self::BIT_SET_BASE_ADDR + (self.as_ptr().addr() - Self::PERIPHERALS_BASE_ADDR);
+        unsafe { (addr as *mut u32).write_volatile(mask) };
+    }
+
+    fn sc_clear(&self, mask: u32) {
+        let addr = Self::BIT_CLEAR_BASE_ADDR + (self.as_ptr().addr() - Self::PERIPHERALS_BASE_ADDR);
+        unsafe { (addr as *mut u32).write_volatile(mask) };
+    }
+}
